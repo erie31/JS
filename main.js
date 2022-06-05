493761aeb74f2b31df2752fd4decd924d7b3ca6d
Erié <erie31@gmail.com>
2022-06-05 18:48:57 -0300

    desafio complementario

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-for (let i = 10; i > 0 ; i--) {
-    console.warn("TheFinalCountDown", i);
-    prompt ("Bip!");
-    if (i == 1){
-        prompt ("Pum!")
-}
-}
+
+const comboBochinche = [];
+let   cantidad     = 4;
+
+do{
+   let entrada = prompt("Arma tu combo, un item a la vez");
+   comboBochinche.push(entrada.toUpperCase());
+   console.log(comboBochinche.length);
+}while(comboBochinche.length != cantidad)
+
+const nuevoCombo = comboBochinche.concat(["¡Consultar por Catering!","Envio sin cargo"]);
+
+alert(nuevoCombo.join("\n"));
